f20befcf32f8f81f37d845215c0b3ee3a89dd144
gourangpathak <gourangp29@gmail.com>
2023-05-13 08:12:52 +0530

    Lab 8

--- Misc/CS311 - Compiler Design/Assignments/6/1904119_lab6/InputFile.cc
+++
@@ -1,6 +0,0 @@
-int main()
-{
-    int a, b;
-    a=6;
-    b=a*7;
-}
--- Misc/CS311 - Compiler Design/Assignments/6/1904119_lab6/customInputFile.cc
+++
@@ -1,9 +0,0 @@
-int main(){
-    int a;
-    float b, c;
-    a = 4.5;        
-    c = b + c;      
-    a = b + 6.5;    
-    if(a>b){ a = b + c;}else{ a = b - c; }   
-    while(a<b){ a = a+c+d;}                  
-}
